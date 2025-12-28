f3f0504b878f49fce451b0bca535c71643f1e98a
Josef Weldemariam <j.weldemariam@web.de>
2025-12-28 05:59:39 +0100

    poll in dev-mode for hot code replacement

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  return {
     plugins: [react()],
     server: {
-        port: 5173,
-        open: true,
-        host: true,
+      port: 5173,
+      open: true,
+      host: true,
+      allowedHosts: ['frontend', 'localhost'],
+      watch: {
+        usePolling: mode === 'development',
+      },
     },
     // ✨ Wichtig für React Router:
     build: {
-        outDir: 'dist',
-        sourcemap: false,
+      outDir: 'dist',
+      sourcemap: false,
     },
     // Diese Zeile sorgt dafür, dass /login, /register usw. an index.html weitergeleitet werden
     //appType: 'spa' sagt Vite, dass es sich um eine Single Page Application handelt,
     // und alle Routen an index.html geschickt werden sollen.
     appType: 'spa',
-})
+  };
+});
